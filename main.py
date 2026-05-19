661627bed7bba4c8704dac807a05f357592bfd44
Yeisner David Giraldo <yeisnergiraldo@gmail.com>
2026-05-19 16:43:28 -0500

    Desarrollo lógica priorización automática de tareas

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ class Tarea:
         self.materia = materia
         self.estado = "Pendiente"
 
-
     def completar(self):
-
         self.estado = "Completada"
+
+    def calcular_dias_restantes(self):
+        return 2
 
 tarea1 = Tarea(1,"Parcial POO","Programación")
 
@@ -20,3 +21,21 @@ tarea1 = Tarea(1,"Parcial POO","Programación")
 print("Nombre:", tarea1.nombre)
 print("Materia:", tarea1.materia)
 print("Estado:", tarea1.estado)
+
+class Priorizador:
+
+
+    @staticmethod
+
+    def asignar_prioridad(tarea):
+        dias = tarea.calcular_dias_restantes()
+        if dias <=2:
+            return "Alta"
+        elif dias <=7:
+            return "Media"
+        else:
+            return "Baja"
+
+prioridad = Priorizador.asignar_prioridad(tarea1)
+print("Prioridad:", prioridad)
+
