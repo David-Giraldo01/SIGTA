bf32d5eaf1e20d0df379441ac287fda10f508eee
Yeisner David Giraldo <yeisnergiraldo@gmail.com>
2026-05-18 20:23:38 -0500

    Implementación clase Tarea con atributos y métodos

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from datetime import datetime, date
+from datetime import date
 
-print("SIGTA - Sistema Inteligente de Gestión de Tareas Académicas")
+class Tarea:
+
+    def __init__(self, id, nombre, materia):
+
+        self.id = id
+        self.nombre = nombre
+        self.materia = materia
+        self.estado = "Pendiente"
+
+
+    def completar(self):
+
+        self.estado = "Completada"
+
+tarea1 = Tarea(1,"Parcial POO","Programación")
+
+
+print("Nombre:", tarea1.nombre)
+print("Materia:", tarea1.materia)
+print("Estado:", tarea1.estado)
