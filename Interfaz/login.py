1b781f3d4744aab543178c6ae1db3fa6fe267c6f
Yeisner David Giraldo <yeisnergiraldo@gmail.com>
2026-05-20 18:30:34 -0500

    login.py creacion de el primer def (__init__)

---
+++ Interfaz/login.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+
+from Model.usuario import Usuario
+from interfaz.ventana_principal import VentanaPrincipal
+
+
+class Login:
+    def __init__(self):
+        self.usuario = Usuario()
+
+        self.ventana_login = tk.Tk()
+        self.ventana_login.title("SIGTA - Inicio de sesión")
+        self.ventana_login.geometry("350x250")
+        self.ventana_login.resizable(False, False)
+
+        self.crear_interfaz_login()
+
